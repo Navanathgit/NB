379483c9aa968ebcbe8fa8194019caca54f0250c
Navanathgit <114656357+Navanathgit@users.noreply.github.com>
2022-10-06 09:45:13 +0530

    Add files via upload

---
+++ TuranaryOperator.java
@@ -0,0 +1,15 @@
+class TuranaryOperator{
+public static void main(String args[]){
+int a=200;
+int b=500;
+int c=400;
+int d=(b>a)?b:a;
+int result=(d>c)?d:c;
+System.out.println("Gretest no. is  "+result);//500
+
+
+
+}
+
+
+}
---
+++ Welcome1.java
@@ -0,0 +1,5 @@
+class Welcome1{
+public static void main(String[] args){
+System.out.println("Welcome to the Xworkz....! "+args[0]+"  "+args[1]);
+}
+}
